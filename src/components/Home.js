68533280de8dd83942471c108a8b2554b5e8e97b
Alejandro Hernandez <alejandro.hernandez@yalochat.com>
2022-04-07 22:06:39 -0500

    Home Screen section structure

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,7 @@
 import styled from 'styled-components'
+import Leftside from './Leftside'
+import Main from './Main'
+import Rightside from './Rightside'
 
 const Home = (props) => {
     return (
@@ -9,6 +12,11 @@ const Home = (props) => {
                 </h5>
                 <p>Find talented pros in record time with Upwork and keep business moving.</p>
             </Section>
+            <Layout>
+                <Leftside />
+                <Main />
+                <Rightside />
+            </Layout>
         </Container>
     )
 }
@@ -45,6 +53,21 @@ const Section = styled.section`
     font-weight: 600;
   }
   @media (max-width: 768px) {
+    flex-direction: column;
+    padding: 0 5px;
+  }
+`;
+
+const Layout = styled.div`
+  display: grid;
+  grid-template-areas: "leftside main rightside";
+  grid-template-columns: minmax(0, 5fr) minmax(0, 12fr) minmax(300px, 7fr);
+  column-gap: 25px;
+  row-gap: 25px;
+  /* grid-template-row: auto; */
+  margin: 25px 0;
+  @media (max-width: 768px) {
+    display: flex;
     flex-direction: column;
     padding: 0 5px;
   }
