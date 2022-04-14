104b33a29f10b7bd93f6d2deb4e8080775750267
Alejandro Hernandez <alejandrohernandez@MacBook-Pro-de-Macbook.local>
2022-04-13 22:26:22 -0500

    Replace name placeholders with active users name

--- src/components/Leftside.js
+++ src/components/Leftside.js
@@ -1,3 +1,4 @@
+import { connect } from 'react-redux';
 import styled from 'styled-components'
 
 const Leftside = (props) => {
@@ -7,7 +8,9 @@ const Leftside = (props) => {
                     <CardBackground />
                     <a>
                         <Photo />
-                        <Link>Welcome, there!</Link>
+                        <Link>
+                          Welcome, {props.user ? props.user.displayName : 'there'}!
+                        </Link>
                     </a>
                     <a>
                         <AddPhotoText>Add a photo</AddPhotoText>
@@ -193,4 +196,10 @@ const CommunityCard = styled(ArtCard)`
   }
 `;
 
-export default Leftside;
+const mapStateToProps = (state) => {
+  return {
+    user: state.userState.user,
+  }
+}
+
+export default connect(mapStateToProps)(Leftside)
