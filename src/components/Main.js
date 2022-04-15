d2bca3893dcf7d6db0d5297183a2ec44ad905125
Alejandro Hernandez <alejandrohernandez@MacBook-Pro-de-Macbook.local>
2022-04-15 08:28:58 -0500

    Create post modal

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,32 @@
+import { useState } from 'react';
 import styled from 'styled-components'
+import PostModal from './PostModal';
 
 const Main = (props) => {
+    const [showModal, setShowModal] = useState('close')
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        if (e.target !== e.currentTarget) {
+            return;
+        }
+        switch (showModal) {
+            case 'open':
+                setShowModal('close');
+                break;
+            case 'close':
+                setShowModal('open');
+                break;
+            default:
+                setShowModal('close');
+                break;
+        }
+    }
     return <Container>
         <ShareBox>Share
         <div>
             <img src="/images/user.svg" alt="" />
-            <button>Start a post</button>
+            <button onClick={handleClick}>Start a post</button>
         </div>
 
         <div>
@@ -83,6 +104,7 @@ const Main = (props) => {
                 </SocialActions>
             </Article>
         </div>
+        <PostModal showModal={showModal} handleClick={handleClick}/>
     </Container>
 }
 
@@ -154,7 +176,7 @@ const ShareBox = styled(CommonCard)`
             }
         }
     }
-`
+`;
 
 const Article = styled(CommonCard)`
     padding: 0;
